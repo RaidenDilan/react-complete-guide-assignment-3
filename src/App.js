c84f35b13295fd4cc431f7ea92e377ed9c761031
RaidenDilan <raiden18@me.com>
2020-02-15 20:33:42 +0000

    Load the Course component as a nested component of Courses

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import { Route, NavLink, Switch } from 'react-router-dom';
 
 import Courses from './containers/Courses/Courses';
 import Users from './containers/Users/Users';
-import Course from './containers/Course/Course';
+// import Course from './containers/Course/Course';
 
 class App extends Component {
   render () {
     return (
       <div className='App'>
         <ol style={ { textAlign: 'left' } }>
-          <li>Load the Course component as a nested component of Courses</li>
           <li>Add a 404 error page and render it for any unknown routes</li>
           <li>Redirect requests to /all-courses to /courses (Your Courses page)</li>
         </ol>
@@ -26,7 +25,7 @@ class App extends Component {
         </nav>
         <Switch>
           <Route path='/users' component={ Users } />
-          <Route path='/courses/:courseId' component={ Course } />
+          { /* <Route path='/courses/:courseId' component={ Course } /> */ }
           <Route path='/courses' component={ Courses } />
         </Switch>
       </div>
