38fbeebfb45ccc89e7cd5a2c3d29a73a6234b41e
RaidenDilan <raiden18@me.com>
2020-02-15 20:10:51 +0000

    Passed the course ID to the Course page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
-import { Route, NavLink } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 
 import Courses from './containers/Courses/Courses';
 import Users from './containers/Users/Users';
+import Course from './containers/Course/Course';
 
 class App extends Component {
   render () {
     return (
       <div className='App'>
         <ol style={ { textAlign: 'left' } }>
-          <li>Pass the course ID to the Course page and output it there</li>
-          <li>Pass the course title to the Course page - pass it as a param or score bonus points by passing it as query params (you need to manually parse them though!)</li>
+          <li>Passed the course title to the Course page - passed it as a param + bonus points by passing it as query params (you need to manually parse them though!)</li>
           <li>Load the Course component as a nested component of Courses</li>
           <li>Add a 404 error page and render it for any unknown routes</li>
           <li>Redirect requests to /all-courses to /courses (Your Courses page)</li>
@@ -25,8 +25,11 @@ class App extends Component {
             </li>
           </ul>
         </nav>
-        <Route path='/courses' component={ Courses } />
-        <Route path='/users' component={ Users } />
+        <Switch>
+          <Route path='/users' component={ Users } />
+          <Route path='/courses/course' component={ Course } />
+          <Route path='/courses' component={ Courses } />
+        </Switch>
       </div>
     );
   }
